726d41d9ac7d8ce2633e1398c7893a4fea3f2b5e
Emanuel Camarena <emanuel.ortega@hotmail.com>
2022-12-24 20:12:55 -0600

    Added basic navbar

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -6,7 +6,7 @@ export const CartWidget = () => {
         <div className='col-xl-3'>
             {/* <img src={imagen} alt="imagen" /> */}
             {/* <img src="/vite.svg" alt="" /> */}
-            <i><AiFillShopping size={40}/></i>
+            <i><AiFillShopping size={40} color={'orange'} onMouseOver={({target})=>target.style.color="white"} onMouseOut={({target})=>target.style.color="orange"}/></i>
             {/* <img width={'50px'} src="https://imgs.search.brave.com/SUy4vxiivPYcycORbkjxAlqc_zngoZK67B_WjCKHdY0/rs:fit:889:1200:1/g:ce/aHR0cHM6Ly9jZG4u/c2hvcGlmeS5jb20v/cy9maWxlcy8xLzE0/OTkvMTg1OC9wcm9k/dWN0cy8xMDY3X0Nh/cnJpdG9Qcm9kdWN0/b3NfMTIwMHgxMjAw/LnBuZz92PTE1Njkx/NDQwNDc" alt="carrito"/> */}
         </div>
     )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Ema' Shop Logo</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
